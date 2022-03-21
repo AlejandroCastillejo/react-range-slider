5d081e25cacb5be829482258b2b2266581c8b6d0
Alejandro Castillejo <ale.acc@hotmail.com>
2022-03-21 20:21:06 +0100

    import regenerator-runtime

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'regenerator-runtime/runtime.js';
+
 import App from './App';
 
 ReactDOM.render(
